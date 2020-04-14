953f77bfb6603deef713ca34cc3927e662fa1bff
Ataias Pereira Reis <ataiasreis@gmail.com>
2020-04-14 07:07:40 -0300

    Implement JackEnPoy game logic
    - Win
    - Draw
    - Lose

--- JackEnPoy/JackEnPoy/ContentView.swift
+++ JackEnPoy/JackEnPoy/ContentView.swift
@@ -8,11 +8,42 @@
 
 import SwiftUI
 
+struct Score {
+    var points: Int
+    var totalGames: Int
+    let maxGames: Int
+}
+
+enum GameResult {
+    case Win
+    case Lose
+    case Draw
+}
+
+enum JackEnPoy: String {
+    case Rock
+    case Paper
+    case Scissors
+
+    func win(against: JackEnPoy) -> GameResult {
+        switch (self, against) {
+        case (.Rock, .Scissors), (.Scissors, .Paper), (.Paper, .Rock):
+            return .Win
+        default:
+            return self == against ? .Draw : .Lose
+
+        }
+    }
+}
+
 struct ContentView: View {
-    let moves = ["Rock", "Paper", "Scissors"]
+    let moves = [JackEnPoy.Rock, .Paper, .Scissors]
 
     @State private var correctAnswer = Int.random(in: 0...2)
     @State private var shouldWin = true
+    @State private var score = Score(points: 0, totalGames: 0, maxGames: 10)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
 
     var body: some View {
         VStack(spacing: 20) {
@@ -20,26 +51,67 @@ struct ContentView: View {
                 .titled()
 
             if shouldWin {
-                Text("You should win")
+                Text("To win you should win")
             } else {
-                Text("You should lose")
+                Text("To win you should lose")
             }
 
             Spacer()
             VStack(spacing: 30) {
                 ForEach(0 ..< moves.count) { n in
                     Button(action: {
-                        // do something
+                        self.update(forUserMove: self.moves[n])
                     }) {
-                        MoveImage(image: self.moves[n])
+                        MoveImage(image: self.moves[n].rawValue)
+                    }
+                    .alert(isPresented: self.$showingScore) {
+                        Alert(
+                            title: Text(self.scoreTitle),
+                            message: Text("Points: \(self.score.points); Game# \(self.score.totalGames)"),
+                            dismissButton: .default(Text("Continue")) {
+                                self.prepareNewGame()
+                            })
                     }
                 }
             }
-//            Score(wins: wins, count: count)
+            ScoreView(score)
 
         }
         .gradiented()
         .foregroundColor(.white)
+    }
+
+    func prepareNewGame() {
+        self.correctAnswer = Int.random(in: 0...2)
+        //        self.shouldWin = ...
+        self.showingScore = false
+    }
+
+    func update(forUserMove userMove: JackEnPoy) {
+        var points = score.points
+        let totalGames = score.totalGames + 1
+
+        let title = "You chose \(userMove) and the opponent chose \(moves[correctAnswer])"
+
+        let gameResult = userMove.win(against: moves[correctAnswer])
+
+        switch (gameResult, shouldWin) {
+        case (.Win, shouldWin), (.Lose, !shouldWin):
+            points += 1
+            self.scoreTitle = "You win! " + title
+        case (.Draw, _):
+            self.scoreTitle = "Game was a draw!"
+        default:
+            self.scoreTitle = "You lose! " + title
+        }
+
+        self.score = Score(
+            points: points,
+            totalGames: totalGames,
+            maxGames: score.maxGames
+        )
+        self.showingScore = true
+
     }
 }
 
@@ -64,12 +136,18 @@ struct Title: ViewModifier {
     }
 }
 
-struct Score: View {
-    let wins: Int
-    let count: Int
+struct ScoreView: View {
+    let score: Score
+
+    init(_ score: Score) {
+        self.score = score
+    }
 
     var body: some View {
-        Text("Score: \(wins)/\(count)")
+        VStack {
+            Text("Points: \(score.points)")
+            Text("Game#: \(score.totalGames)/\(score.maxGames)")
+        }
     }
 }
 
@@ -86,6 +164,11 @@ struct BackgroundModifier: ViewModifier {
     }
 }
 
+extension Bool {
+    static func ^ (left: Bool, right: Bool) -> Bool {
+        return left != right
+    }
+}
 
 extension View {
     func titled() -> some View {
