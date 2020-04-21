ac824382071890f349e2f9c095604d4e004fe19d
Ataias Pereira Reis <ataiasreis@gmail.com>
2020-04-21 17:56:26 -0300

    Add gestures animations

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -10,7 +10,67 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ExplicitAnimations()
+        CoolStringAnimation()
+    }
+}
+
+struct CoolStringAnimation: View {
+    let letters = Array("Hello SwiftUI")
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count) { num in
+                Text(String(self.letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(self.enabled ? Color.blue : Color.red)
+                    .offset(self.dragAmount)
+                    .animation(Animation.default.delay(Double(num) / 20))
+            }
+        }
+        .gesture( DragGesture()
+        .onChanged { self.dragAmount = $0.translation }
+        .onEnded { _ in
+            self.dragAmount = .zero
+            self.enabled.toggle()
+        })
+    }
+}
+
+struct GesturesCard: View {
+    @State private var dragAmount = CGSize.zero
+
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .offset(dragAmount)
+        .gesture(DragGesture()
+            .onChanged { self.dragAmount = $0.translation }
+            .onEnded { _ in
+                withAnimation(.spring()) {
+                    self.dragAmount = .zero
+                }
+            }
+        )
+    }
+}
+
+struct AnimationStack: View {
+    @State private var enabled = false
+
+    var body: some View {
+        Button("Tap Me") {
+            self.enabled.toggle()
+        }
+        .frame(width: 200, height: 200)
+        .background(enabled ? Color.blue : Color.red)
+        .animation(nil)
+        .foregroundColor(.white)
+        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+        .animation(.interpolatingSpring(stiffness: 10, damping: 1))
     }
 }
 
