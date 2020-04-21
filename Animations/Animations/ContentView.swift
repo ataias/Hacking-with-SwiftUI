b3f3d3b3a1c5dfd4a823498be4b79312e7f4b128
Ataias Pereira Reis <ataiasreis@gmail.com>
2020-04-21 18:10:45 -0300

    Add custom transition using ViewModifier

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,6 +8,25 @@
 
 import SwiftUI
 
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         Transitions()
@@ -29,7 +48,7 @@ struct Transitions: View {
                 Rectangle()
                     .fill(Color.red)
                     .frame(width: 200, height: 200)
-                    .transition(.scale)
+                    .transition(.pivot)
             }
         }
     }
