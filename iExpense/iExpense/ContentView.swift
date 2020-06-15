2b345aeebb34a59b90777aaa9ba01b6414132ceb
Ataias Pereira Reis <ataiasreis@gmail.com>
2020-06-15 07:26:50 -0300

    Add basic iExpense functionality
    - Add items
    - Save on UserDefaults and load from them

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -8,9 +8,65 @@
 
 import SwiftUI
 
+struct ExpenseItem: Identifiable, Codable {
+    let id = UUID() // Notice the UUID is automatically assigned!
+    let name: String
+    let type: String
+    let amount: Int
+}
+
+class Expenses: ObservableObject {
+    init() {
+        let decoder = JSONDecoder()
+
+        if let data = UserDefaults.standard.data(forKey: "Items") {
+            if let items = try? decoder.decode([ExpenseItem].self, from: data) {
+                self.items = items
+                return
+            }
+        }
+
+        self.items = []
+    }
+
+    @Published var items: [ExpenseItem] {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+}
+
 struct ContentView: View {
+    @ObservedObject var expenses = Expenses()
+    @State private var showingAddExpense = false
+
     var body: some View {
-        CodableView()
+        NavigationView {
+            List {
+                ForEach(expenses.items) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationBarTitle("iExpense")
+            .navigationBarItems(trailing:
+                Button(action: {
+                    self.showingAddExpense.toggle()
+                }) {
+                    Image(systemName: "plus")
+                }
+            )
+        }
+        .sheet(isPresented: $showingAddExpense) {
+            AddView(expenses: self.expenses)
+        }
+    }
+
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
