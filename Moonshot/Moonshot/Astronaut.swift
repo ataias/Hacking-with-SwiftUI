8a4b9c829ec4c5a734af95f6ae7051702799e9b6
Ataias Pereira Reis <ataiasreis@gmail.com>
2020-06-20 13:57:36 -0300

    Add astronauts json data

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,13 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Ataias Pereira Reis on 20/06/20.
+//  Copyright © 2020 ataias. All rights reserved.
+//
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
