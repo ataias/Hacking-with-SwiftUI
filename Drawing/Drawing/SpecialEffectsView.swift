00a7d93ea8447917e75b7f3f1cba8bcfb46586fa
Ataias Pereira Reis <ataiasreis@gmail.com>
2020-06-21 17:44:30 -0300

    Animate shape

--- Drawing/Drawing/SpecialEffectsView.swift
+++ Drawing/Drawing/SpecialEffectsView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct SpecialEffectsView: View {
     var body: some View {
-        BlurSaturationView()
+        AnimatableDataView()
     }
 }
 
@@ -81,6 +81,39 @@ struct ScreenBlendModeView: View {
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color.black)
         .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct AnimatableDataView: View {
+    @State private var insetAmount: CGFloat = 50
+    var body: some View {
+        Trapezoid(insetAmount: insetAmount)
+            .frame(width: 200, height: 100)
+            .onTapGesture {
+                withAnimation {
+                    self.insetAmount = CGFloat.random(in: 10...90)
+                }
+            }
+    }
+}
+struct Trapezoid: Shape {
+    var insetAmount: CGFloat
+
+    var animatableData: CGFloat {
+        get { insetAmount }
+        set { self.insetAmount = newValue }
+    }
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+
+        return path
     }
 }
 
