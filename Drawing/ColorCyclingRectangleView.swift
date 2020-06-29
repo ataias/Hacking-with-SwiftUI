64346e034fbb75ff5859f602d727f57f38ff4250
Ataias Pereira Reis <ataiasreis@gmail.com>
2020-06-29 05:56:22 -0300

    Add ColorCyclingRectangleView (incomplete)

---
+++ Drawing/ColorCyclingRectangleView.swift
@@ -0,0 +1,45 @@
+//
+//  ColorCyclingRectangleView.swift
+//  Drawing
+//
+//  Created by Ataias Pereira Reis on 29/06/20.
+//  Copyright © 2020 ataias. All rights reserved.
+//
+
+import SwiftUI
+
+struct ColorCyclingRectangleView: View {
+    var amount = 0.0
+    var steps = 100
+
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Rectangle()
+                    .inset(by: CGFloat(value))
+                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
+                        self.color(for: value, brightness: 1),
+                        self.color(for: value, brightness: 0.5)
+                    ]), startPoint: .top, endPoint: .bottom), lineWidth: 10)
+                .offset(x: 2, y: 5)
+            }
+        }
+        .drawingGroup()
+    }
+
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
+
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
+struct ColorCyclingRectangleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorCyclingRectangleView()
+    }
+}
