5454115e76b9c84f8d0efce08cb66553d9e60e59
Ataias Pereira Reis <ataiasreis@gmail.com>
2020-07-13 06:06:27 -0300

    Add candy relationships

---
+++ CoreDataProject/CoreDataProject/Country+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Country+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Ataias Pereira Reis on 12/07/20.
+//  Copyright © 2020 ataias. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Country)
+public class Country: NSManagedObject {
+
+}
