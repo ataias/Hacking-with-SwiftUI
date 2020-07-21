0ce94f3738a3ba7a5d8760e9a519c809981aa977
Ataias Pereira Reis <ataiasreis@gmail.com>
2020-07-21 06:20:57 -0300

    Apply sepia filter and rotate image

--- Instafilter/Instafilter/CoreImageTestView.swift
+++ Instafilter/Instafilter/CoreImageTestView.swift
@@ -9,6 +9,7 @@
 import SwiftUI
 import CoreImage
 import CoreImage.CIFilterBuiltins
+import UIKit
 
 struct CoreImageTestView: View {
     @State private var image: Image?
@@ -34,12 +35,39 @@ struct CoreImageTestView: View {
         currentFilter.inputImage = beginImage
         currentFilter.intensity = 1
 
-        // TODO Continue here
+        guard let outputImage = currentFilter.outputImage else { return }
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg).rotate(radians: .pi/2)
+            image = Image(uiImage: uiImage)
+        }
     }
 }
 
 struct CoreImageTestView_Previews: PreviewProvider {
     static var previews: some View {
         CoreImageTestView()
+    }
+}
+
+extension UIImage {
+    func rotate(radians: CGFloat) -> UIImage {
+        let rotatedSize = CGRect(origin: .zero, size: size)
+            .applying(CGAffineTransform(rotationAngle: CGFloat(radians)))
+            .integral.size
+        UIGraphicsBeginImageContext(rotatedSize)
+        if let context = UIGraphicsGetCurrentContext() {
+            let origin = CGPoint(x: rotatedSize.width / 2.0,
+                                 y: rotatedSize.height / 2.0)
+            context.translateBy(x: origin.x, y: origin.y)
+            context.rotate(by: radians)
+            draw(in: CGRect(x: -origin.y, y: -origin.x,
+                            width: size.width, height: size.height))
+            let rotatedImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+
+            return rotatedImage ?? self
+        }
+
+        return self
     }
 }
