8e0fd09d5077c1d0c162aca80fa931d334b99ecf
Ataias Pereira Reis <ataiasreis@gmail.com>
2020-07-23 07:25:52 -0300

    Add basic filter functionality and show results

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -7,6 +7,8 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
     @State private var image: Image?
@@ -15,54 +17,83 @@ struct ContentView: View {
     @State private var filterIntensity = 0.5
     @State private var showingImagePicker = false
 
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
+
     var body: some View {
-        NavigationView {
-            VStack {
-                ZStack {
-                    Rectangle()
-                        .fill(Color.secondary)
-                    if image != nil {
-                        image?
-                        .resizable()
-                        .scaledToFit()
-                    } else {
-                        Text("Tap to select a picture")
-                            .foregroundColor(.white)
-                            .font(.headline)
+        let intensity = Binding<Double>(
+            get: {
+                self.filterIntensity
+            },
+            set: {
+                self.filterIntensity = $0
+                self.applyProcessing()
+            }
+        )
+
+        return (
+            NavigationView {
+                VStack {
+                    ZStack {
+                        Rectangle()
+                            .fill(Color.secondary)
+                        if image != nil {
+                            image?
+                                .resizable()
+                                .scaledToFit()
+                        } else {
+                            Text("Tap to select a picture")
+                                .foregroundColor(.white)
+                                .font(.headline)
+                        }
+                    }
+                    .onTapGesture {
+                        self.showingImagePicker = true
+                    }
+
+                    HStack {
+                        Text("Intensity")
+                        Slider(value: intensity, in: 0.0...1.0)
+                    }.padding(.vertical)
+
+                    HStack {
+                        Button("Change Filter") {
+                            // TODO Change filter
+                        }
+
+                        Spacer()
+
+                        Button("Save") {
+                            // TODO Save the picture
+                        }
                     }
                 }
-                .onTapGesture {
-                    self.showingImagePicker = true
-                }
-
-                HStack {
-                    Text("Intensity")
-                    Slider(value: $filterIntensity, in: 0.0...1.0)
-                }.padding(.vertical)
-
-                HStack {
-                    Button("Change Filter") {
-                        // TODO Change filter
-                    }
-
-                    Spacer()
-
-                    Button("Save") {
-                        // TODO Save the picture
-                    }
+                .padding([.horizontal, .bottom])
+                .navigationBarTitle("Instafilter")
+                .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+                    ImagePicker(image: self.$inputImage)
                 }
             }
-            .padding([.horizontal, .bottom])
-            .navigationBarTitle("Instafilter")
-            .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
-                ImagePicker(image: self.$inputImage)
-            }
-        }
+        )
     }
 
     func loadImage() {
         guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
+        let beginImage = CIImage(image: inputImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+
+        guard let outputImage = currentFilter.outputImage else { return }
+
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+        }
+
     }
 }
 
