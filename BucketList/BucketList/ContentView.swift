ab6562d58937dffd6296b765cbd8e676f893da7a
Ataias Pereira Reis <ataiasreis@gmail.com>
2020-07-26 16:46:52 -0300

    Add initial interactions between map data and app

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -10,8 +10,52 @@ import SwiftUI
 import MapKit
 
 struct ContentView: View {
+    @State private var centerCoordinate = CLLocationCoordinate2D()
+    @State private var locations = [MKPointAnnotation]()
+    @State private var selectedPlace: MKPointAnnotation?
+    @State private var showingPlaceDetails = false
+
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            MapView(centerCoordinate: $centerCoordinate, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails, annotations: locations)
+                .edgesIgnoringSafeArea(.all)
+            Circle()
+                .fill(Color.blue)
+                .opacity(0.3)
+                .frame(width: 32, height: 32)
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        let newLocation = MKPointAnnotation()
+                        newLocation.title = "Example title"
+                        newLocation.coordinate = self.centerCoordinate
+                        self.locations.append(newLocation)
+                    }) {
+                        Image(systemName: "plus")
+                    }
+                    .padding()
+                    .background(Color.black.opacity(0.75))
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .clipShape(Circle())
+                    .padding(.trailing)
+                }
+            }
+        }
+        .alert(isPresented: $showingPlaceDetails) {
+            let title = Text(selectedPlace?.title ?? "Unknown")
+            let message = Text(selectedPlace?.subtitle ?? "Missing place information")
+            let primaryButton = Alert.Button.default(Text("OK"))
+            let secondaryButton = Alert.Button.default(Text("Edit")) {
+                // do something
+            }
+
+            return (
+                Alert(title: title, message: message, primaryButton: primaryButton, secondaryButton: secondaryButton)
+            )
+        }
     }
 }
 
