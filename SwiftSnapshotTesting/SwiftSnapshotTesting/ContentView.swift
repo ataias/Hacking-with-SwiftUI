6f67122c2c1c4fcba58f8eab5626b5ee937ac7e4
Ataias Pereira Reis <ataiasreis@gmail.com>
2020-08-08 17:59:19 -0300

    Add initial version of SnapshotTesting

---
+++ SwiftSnapshotTesting/SwiftSnapshotTesting/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftSnapshotTesting
+//
+//  Created by Ataias Pereira Reis on 08/08/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftSnapshotTesting/SwiftSnapshotTesting/README.md
@@ -0,0 +1,3 @@
+#  SwiftSnapshotTesting
+
+This is actually not part of the hacking with swift tutorials, but from a Vadim Bulavim's [tutorial](https://www.vadimbulavin.com/snapshot-testing-swiftui-views/)
