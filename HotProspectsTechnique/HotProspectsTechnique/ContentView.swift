54c6e1fdaf35f02504855e916a52e1f93cb33686
Ataias Pereira Reis <ataiasreis@gmail.com>
2020-08-09 08:02:31 -0300

    Add context menu

--- HotProspectsTechnique/HotProspectsTechnique/ContentView.swift
+++ HotProspectsTechnique/HotProspectsTechnique/ContentView.swift
@@ -35,6 +35,8 @@ struct ContentView: View {
 
     @ObservedObject var updater = DelayedUpdater()
 
+    @State private var backgroundColor = Color.red
+
     var body: some View {
         VStack {
 
@@ -78,6 +80,41 @@ struct ContentView: View {
                     Text("Three")
                 }
                 .tag(2)
+
+                VStack {
+                    Text("Hello, World!")
+                        .padding()
+                        .background(backgroundColor)
+
+                    Text("Change Color")
+                        .padding()
+                        .contextMenu {
+                            Button(action: {
+                                self.backgroundColor = .red
+                            }) {
+                                Text("Red")
+                                Image(systemName: "checkmark.circle.fill")
+                                .foregroundColor(.red)
+                            }
+
+                            Button(action: {
+                                self.backgroundColor = .green
+                            }) {
+                                Text("Green")
+                            }
+
+                            Button(action: {
+                                self.backgroundColor = .blue
+                            }) {
+                                Text("Blue")
+                            }
+                    }
+                }
+                .tabItem {
+                    Image(systemName: "doc.fill")
+                    Text("Four")
+                }
+                .tag(3)
 
             }
         }
