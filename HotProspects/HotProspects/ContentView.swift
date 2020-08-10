1395250ea80d48b7135d4173e53ad817b111010d
Ataias Pereira Reis <ataiasreis@gmail.com>
2020-08-10 06:17:46 -0300

    Add initial code for HotProspects with tab view

---
+++ HotProspects/HotProspects/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Ataias Pereira Reis on 10/08/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            ProspectsView()
+                .tabItem {
+                    Image(systemName: "person.3")
+                    Text("Everyone")
+                }
+            ProspectsView()
+                .tabItem {
+                    Image(systemName: "checkmark.circle")
+                    Text("Contacted")
+                }
+            ProspectsView()
+                .tabItem {
+                    Image(systemName: "questionmark.diamond")
+                    Text("Uncontacted")
+                }
+            MeView()
+                .tabItem {
+                    Image(systemName: "person.crop.square")
+                    Text("Me")
+                }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HotProspects/HotProspects/ProspectsView.swift
@@ -0,0 +1,20 @@
+//
+//  ProspectsView.swift
+//  HotProspects
+//
+//  Created by Ataias Pereira Reis on 10/08/20.
+//
+
+import SwiftUI
+
+struct ProspectsView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ProspectsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProspectsView()
+    }
+}
