3deedcd34aeb8c5ea0e07a2e6d75f1f557d46d27
Ataias Pereira Reis <ataiasreis@gmail.com>
2020-08-25 07:34:31 -0300

    Add dice roll function

--- DiceRoll/DiceRoll/ContentView.swift
+++ DiceRoll/DiceRoll/ContentView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var selectedTab = 0
+    @State private var pendingRolls = 0
+    @State private var currentNumber = 5
+    @State private var sides = 10
+
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
 
     var body: some View {
         VStack {
             TabView(selection: $selectedTab) {
                 VStack {
                     Text("Tab 1 - Show Only")
-                    DiceView(number: 5)
+                    DiceView(number: currentNumber)
+                    Button(action: start) {
+                        Text("Play")
+                    }
                 }
                 .onTapGesture {
                     self.selectedTab = 1
@@ -34,12 +42,26 @@ struct ContentView: View {
                     Text("Score")
                 }
                 .tag(1)
-
-
-
             }
+            .onReceive(timer, perform: roll)
         }
 
+    }
+
+    func start() {
+        pendingRolls = 5
+    }
+
+    func roll(_ date: Date) {
+        guard pendingRolls > 0 else { return }
+        var next = Int.random(in: 1...sides)
+        while next == currentNumber {
+            next = Int.random(in: 1...sides)
+        }
+        withAnimation {
+            currentNumber = next
+        }
+        pendingRolls -= 1
     }
 }
 
