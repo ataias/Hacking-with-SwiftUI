43afd3a900de75b17abcc45e160b1ccbfdbf3b03
Ataias Pereira Reis <ataiasreis@gmail.com>
2020-08-25 07:17:31 -0300

    Add TabView

--- DiceRoll/DiceRoll/ContentView.swift
+++ DiceRoll/DiceRoll/ContentView.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedTab = 0
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            TabView(selection: $selectedTab) {
+                VStack {
+                    Text("Tab 1 - Show Only")
+                    DiceView(number: 5)
+                }
+                .onTapGesture {
+                    self.selectedTab = 1
+                }
+                .tabItem {
+                    Image(systemName: "play")
+                    Text("Play")
+                }
+                .tag(0)
+
+                VStack {
+                    Text("Tab 2 - Edit View")
+                }
+                .tabItem {
+                    Image(systemName: "gamecontroller")
+                    Text("Score")
+                }
+                .tag(1)
+
+
+
+            }
+        }
+
     }
 }
 
